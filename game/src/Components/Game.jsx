cbc042255dfb177497934f6a18a4195a1de21f94
Ivanna _ <ivantina@gmail.com>
2024-06-02 19:41:51 +0200

    commit

--- game/src/Components/Game.jsx
+++ game/src/Components/Game.jsx
@@ -10,9 +10,34 @@ const Game = () => {
     const [xIsNext, setXIsNext] = useState(true)
     const winner = calculateWinner(board) 
 
+
+    const handleClick = (index) => {
+        const boardCopy = [...board]
+
+        if (winner || boardCopy[index])
+            return
+
+        boardCopy[index] = xIsNext ? 'X' : 'O'
+
+        setBoard(boardCopy)
+        setXIsNext(!xIsNext)
+
+    }
+
+    const startNewGame =() => {
+        return(
+            <button className='start_btn' onClick ={() => setBoard (Array(9).fill(null))}>
+                Очистить поле
+            </button>
+        )
+    }
     return (
         <div className='wrapper'>
-            <Board squares = {board}/>
+            { startNewGame()}
+            <Board squares = {board} click={handleClick}/>
+            <p>
+                {winner ? "победитель" + winner : "Сейчас ходит" + (xIsNext ? 'X' : 'O')}
+            </p>
         </div>
     )
 }
